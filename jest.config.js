462d41de0f095124316c158ac15f754b40bf2a4e
Vagner S <vagners.tambalo@gmail.com>
2021-01-05 14:23:40 -0300

    apps: add another-app

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  projects: ['<rootDir>/apps/app', '<rootDir>/libs/components'],
+  projects: [
+    '<rootDir>/apps/app',
+    '<rootDir>/libs/components',
+    '<rootDir>/apps/another-app',
+  ],
 };
